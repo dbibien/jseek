53f4a3f313b87f0ccc150239571cfd8c65eba57c
Bibi <dbibien@gmail.com>
2023-04-14 16:06:28 -0400

    Added description, qualifications and, responsibilities sections. Also styled them

--- src/component/JobCardDetail/JobCardDetail.tsx
+++ src/component/JobCardDetail/JobCardDetail.tsx
@@ -15,8 +15,33 @@ const JobCardDetail = (props: jobType) => {
         <p
           className={styles.paddingBottom}
         >{`${props.job?.job_city}, ${props.job?.job_state} ${props.job?.job_country}`}</p>
+        <p className={styles.paddingBottom}>{props.job?.job_employment_type}</p>
+
         <button className={styles.applyButton}>Apply now</button>
         <button className={styles.bookmarkButton}>Bookmark</button>
+      </div>
+
+      <div className={styles.detailsContainer}>
+        <div className={styles.description}>
+          <h4 className={styles.sectionHeading}>Description</h4>
+          <p className={styles.descriptParag}>{props.job?.job_description}</p>
+          <h4 className={styles.sectionHeading}>Qualifiation</h4>
+          <ul className={styles.ul}>
+            {props.job?.job_highlights?.Qualifications?.map((q) => (
+              <li className={styles.listItem} key={q}>
+                {q}
+              </li>
+            ))}
+          </ul>
+          <h4 className={styles.sectionHeading}>Responsibilities</h4>
+          <ul className={styles.ul}>
+            {props.job?.job_highlights?.Responsibilities?.map((r) => (
+              <li className={styles.listItem} key={r}>
+                {r}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
