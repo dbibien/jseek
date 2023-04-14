ce72a0413c33dc03ebf0f03bb16bef11e9c96b93
Bibi <dbibien@gmail.com>
2023-04-14 15:34:44 -0400

    Job detail header is designed

--- src/component/JobCardDetail/JobCardDetail.tsx
+++ src/component/JobCardDetail/JobCardDetail.tsx
@@ -2,9 +2,22 @@ import { jobType } from '../../types';
 import styles from './JobCardDetail.module.css';
 
 const JobCardDetail = (props: jobType) => {
+  if (!props.job) return '';
   return (
     <div className={styles.container}>
-      <h1>{props.job?.job_title}</h1>
+      <div className={styles.header}>
+        <h1 className={styles.paddingBottom}>{props.job?.job_title}</h1>
+        <p className={styles.paddingBottom}>
+          {props.job?.employer_name}
+          {' | Quality: '}
+          {props.job?.job_apply_quality_score * 100.0}
+        </p>
+        <p
+          className={styles.paddingBottom}
+        >{`${props.job?.job_city}, ${props.job?.job_state} ${props.job?.job_country}`}</p>
+        <button className={styles.applyButton}>Apply now</button>
+        <button className={styles.bookmarkButton}>Bookmark</button>
+      </div>
     </div>
   );
 };
